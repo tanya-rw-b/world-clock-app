f712b664db949e384eaf3553b157ceadbe5b88dd
tanya-rw-b <133091241+tanya-rw-b@users.noreply.github.com>
2023-09-21 14:37:53 +0100

    cleaned HTML, connected JS to select menu

--- src/index.js
+++ src/index.js
@@ -2,33 +2,35 @@ function updateTime() {
   // Los Angeles
 
   let losAngelesElement = document.querySelector("#los-angeles");
-  // alternative way: let losAngelesDate = document.querySelector("#los-angeles .date");
-  let losAngelesDate = losAngelesElement.querySelector(".date");
-  losAngelesDate.innerHTML = moment
-    .tz("America/Los_Angeles")
-    .format("MMMM Do YYYY");
-  let losAngelesTemporaryTime = moment
-    .tz("America/Los_Angeles")
-    .format("hh:mm:ss");
-  let losAngelesTemporaryAmPm = moment.tz("America/Los_Angeles").format("A");
+  if (losAngelesElement) {
+    // alternative way: let losAngelesDate = document.querySelector("#los-angeles .date");
+    let losAngelesDate = losAngelesElement.querySelector(".date");
+    losAngelesDate.innerHTML = moment
+      .tz("America/Los_Angeles")
+      .format("MMMM Do YYYY");
+    let losAngelesTemporaryTime = moment
+      .tz("America/Los_Angeles")
+      .format("h:mm:ss");
+    let losAngelesTemporaryAmPm = moment.tz("America/Los_Angeles").format("A");
 
-  let losAngelesTime = losAngelesElement.querySelector(".time");
-  losAngelesTime.innerHTML = `${losAngelesTemporaryTime}<small> ${losAngelesTemporaryAmPm}</small>`;
-
+    let losAngelesTime = losAngelesElement.querySelector(".time");
+    losAngelesTime.innerHTML = `${losAngelesTemporaryTime}<small> ${losAngelesTemporaryAmPm}</small>`;
+  }
   // Sydney
 
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
 
-  sydneyDateElement.innerHTML = moment
-    .tz("Australia/Sydney")
-    .format("MMMM Do YYYY");
-  let sydneyTime = moment().tz("Australia/Sydney");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
-
+    sydneyDateElement.innerHTML = moment
+      .tz("Australia/Sydney")
+      .format("MMMM Do YYYY");
+    let sydneyTime = moment().tz("Australia/Sydney");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   // let sydneyTemporaryTime = moment.tz("Australia/Sydney").format("hh:mm:ss");
   // let sydneyTemporaryAmPm = moment.tz("Australia/Sydney").format("A");
 
@@ -38,9 +40,21 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment.tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = "Please eat more chocolate";
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div class="city-date">
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+
+  setInterval(1000);
 }
 
 updateTime();
