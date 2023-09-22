9028c3e7b800657d4e387d54513ca7517b29482e
tanya-rw-b <133091241+tanya-rw-b@users.noreply.github.com>
2023-09-22 13:34:42 +0100

    added "current location" HTML/JS

--- src/index.js
+++ src/index.js
@@ -40,6 +40,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment.tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -53,8 +56,6 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
         </div>`;
-
-  setInterval(1000);
 }
 
 updateTime();
